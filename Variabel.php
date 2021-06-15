bd11207764592794eac981a878d099544ef70b67
Mushlih Almubarak <mushlih2004@gmail.com>
2021-06-15 11:08:06 +0300

    Menambahkan file variabel

---
+++ Variabel.php
@@ -0,0 +1,12 @@
+<?php
+// Variabel pada PHP
+/* 
+1. Tidak boleh diawali dengan angka, tapi boleh mengandung angka
+2. Tidak boleh ada spasi
+3. Tidak boleh ada tanda minus (-), tapi boleh menggunakan underscore (_)
+*/
+$nama = "Mushlih";
+echo "Nama saya $nama <br>";
+// Jika menggunakan titik dua, maka ia tidak bisa membaca isi variabelnya
+echo 'Nama saya $nama <br>'
+?>
