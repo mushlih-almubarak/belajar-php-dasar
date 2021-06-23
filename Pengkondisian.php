3ea8d222965f506b10a4fcd2e1387beeba749ff4
Mushlih Almubarak <mushlih2004@gmail.com>
2021-06-23 11:03:39 +0300

    Menambahkan file pengkondisian

---
+++ Pengkondisian.php
@@ -0,0 +1,32 @@
+<?php
+// Pengkondisian atau If Else
+/* 
+1. If else
+2. If else If else
+3. Ternary: Untuk if else yang sederhana
+4. Switch: Untuk if else yang rumit, disederhanakan menggunakan metode ini
+*/
+
+$kondisi = 10;
+
+// If else
+if ($kondisi < 20) {
+    echo "Benar";
+}
+else {
+    echo "Salah";
+}
+
+echo "<br>";
+
+// if else if else
+if ($kondisi < 20) {
+    echo "Benar";
+} else if ($kondisi == 30) {
+    echo "Sama";
+}
+else {
+    echo "Salah";
+}
+
+?>
