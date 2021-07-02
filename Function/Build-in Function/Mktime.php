8db3e0fe9d0551d11f193fe8f39d96e993b23b08
Mushlih Almubarak <mushlih2004@gmail.com>
2021-07-02 16:45:26 +0300

    Memperbaiki salah letak parameter

--- Function/Build-in Function/Mktime.php
+++ Function/Build-in Function/Mktime.php
@@ -3,9 +3,9 @@
 // Parameternya (harus berurutan): jam, menit, detik, bulan, tanggal, tahun
 
 // PHP, sudah berapa detik dari tanggal 31 oktober 2004?
-echo mktime(0, 0, 0, 31, 10, 2004);
+echo mktime(0, 0, 0, 10, 31, 2004);
 echo "<br>";
 
 // PHP, pada tanggal 31 oktober 2004 itu hari apa?
-echo date("l", mktime(0, 0, 0, 31, 10, 2004));
+echo date("l", mktime(0, 0, 0, 10, 31, 2004));
 ?>
