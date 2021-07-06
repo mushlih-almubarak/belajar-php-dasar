9f64cd9eec5482c1c123858a4c8cf22163b39fd8
Mushlih Almubarak <mushlih2004@gmail.com>
2021-07-06 10:44:40 +0300

    Membuat file multi array

---
+++ Array/MultiArray.php
@@ -0,0 +1,11 @@
+<?php
+// Membuat banyak array di dalam array
+$arr1 = [
+    ["Mushlih", 17], // Elemen luar 0 
+    ["Hadziq", 10], // Elemen luar 1
+    ["Syakira", 14,] // Elemen luar 2
+];
+
+// Buka elemen luar 2, lalu tampilkan index yang ke 1
+echo $arr1[2][1];
+?>
