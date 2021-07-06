ef5da674bdaf59343eae6de156ae3809ee57726e
Mushlih Almubarak <mushlih2004@gmail.com>
2021-07-06 11:50:27 +0300

    Hapus koma di multiarray

--- Array/MultiArray.php
+++ Array/MultiArray.php
@@ -3,7 +3,7 @@
 $arr1 = [
     ["Mushlih", 17], // Elemen luar 0 
     ["Hadziq", 10], // Elemen luar 1
-    ["Syakira", 14,] // Elemen luar 2
+    ["Syakira", 14] // Elemen luar 2
 ];
 
 // Buka elemen luar 2, lalu tampilkan index yang ke 1
