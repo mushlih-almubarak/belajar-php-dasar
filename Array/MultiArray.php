78e5d27b11da503d3f333d6a53d76397e000cbcb
Mushlih Almubarak <mushlih2004@gmail.com>
2021-07-06 12:01:48 +0300

    Menambahkan contoh di multi array

--- Array/MultiArray.php
+++ Array/MultiArray.php
@@ -5,7 +5,28 @@ $arr1 = [
     ["Hadziq", 10], // Elemen luar 1
     ["Syakira", 14] // Elemen luar 2
 ];
-
 // Buka elemen luar 2, lalu tampilkan index yang ke 1
 echo $arr1[2][1];
+
+echo "<br>";
+
+$arr2 = [
+    [
+        "depan" => "Mushlih",
+        "belakang" => "Almubarak",
+        "nilai" => [90, 95, 85] // Array di dalam 2 array
+    ],
+    [
+        "depan" => "Muhammad",
+        "belakang" => "Alhadziq",
+        "nilai" => [
+                    "akidah" => 98, 
+                    "fikih" => 90, 
+                    "hadis" => 82
+            ]
+    ]
+];
+echo $arr2[0]["nilai"][1];
+echo "<br>";
+echo $arr2[1]["nilai"]["hadis"];
 ?>
