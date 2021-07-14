a942d1add0cb166481258347cc6ab8fe87c53b77
Mushlih Almubarak <mushlih2004@gmail.com>
2021-07-14 11:05:47 +0300

    Menambahkan query untuk ubah data

--- SQL/Query.php
+++ SQL/Query.php
@@ -29,5 +29,26 @@
         $id = $_GET["id"];
         $data_id = mysqli_query($db, "SELECT * FROM mahasiswa WHERE Id = $id");
         $get_id = mysqli_fetch_assoc($data_id);
+        
+        // Query ubah data
+        if (isset($_POST["update"])) {
+            $nama = $_POST["nama"];
+            $email = $_POST["email"];
+            $jurusan = $_POST["jurusan"];
+            $gambar = $_POST["foto"];
+            
+            $query = "UPDATE mahasiswa SET Nama = '$nama', Email = '$email', Jurusan = '$jurusan', Gambar = '$gambar' WHERE id = $id";
+            $update_query = mysqli_query($db, $query);
+            
+                // Mengecek apakah data berhasil di update
+                if (mysqli_affected_rows($db) > 0) {
+                    echo "  <script> alert('Alhamdulillah, Data Berhasil Diubah');
+                                document.location.href = 'index.php'; </script>";
+                } else {
+                    echo "  <script>
+                                alert('Maaf, Data Gagal Diubah');
+                            </script>";
+                }
+                }
     }
 ?>
