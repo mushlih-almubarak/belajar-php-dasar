753e79cd900c567fb3335f64b0354f5c87701b4c
Mushlih Almubarak <mushlih2004@gmail.com>
2021-07-20 11:24:53 +0300

    Membuat file file_exists

---
+++ file_exists.php
@@ -0,0 +1,8 @@
+<?php
+    // Mengecek apakah suatu file ada atau tidak
+    if (file_exists("Hello World.php")) {
+        echo "File Hello World.php ada";
+    } else {
+        echo "File Hello World.php tidak ada";
+    }
+?>
