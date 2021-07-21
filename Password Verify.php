1c55eeb212d2d102198517aa1aec8d42ad724982
Mushlih Almubarak <mushlih2004@gmail.com>
2021-07-21 17:28:46 +0300

    Modifikasi password verify

--- Password Verify.php
+++ Password Verify.php
@@ -1,9 +1,9 @@
 <?php
-    // User menulis password yang biasa, lalu dihash, kemudian dicek apakah hash password tersebut sudah benar dengan password aslinya
-    $password = '$2y$10$S2vwv9f1XRKkeGmsv/rVAOawXJ3TN5h/JAwCvtNW75XP/AGygD5wi'; // Contoh disini menggunakan password setelah di hash
-    $konfirmasi = 'rahasia'; 
+    // User menulis password biasa, lalu dihash, kemudian dicek apakah hash password tersebut sudah benar dengan hash password aslinya
+    $password = 'rahasia';
+    $konfirmasi = '$2y$10$S2vwv9f1XRKkeGmsv/rVAOawXJ3TN5h/JAwCvtNW75XP/AGygD5wi'; // Password setelah di hash
 
-    // password_verify($password_yang_benar, $password_yang_ditulis_user);
-    $hasil = password_verify($konfirmasi, $password);
+    // password_verify($password_yang_ditulis_user, $password_yang_benar);
+    $hasil = password_verify($password, $konfirmasi);
     var_dump($hasil);
 ?>
