76688e4511536632f6ae00586b3442984cf1608a
Mushlih Almubarak <mushlih2004@gmail.com>
2021-07-22 11:24:33 +0300

    Membuat folder cookie

---
+++ Cookie/Cookie.php
@@ -0,0 +1,4 @@
+<?php
+    // Membuat cookie dgn parameter: "nama (key)", "Isi cookie (seperti variabel)", "berapa lama cookie berlaku".
+    setcookie("nama", "Mushlih", time()+60); // Disini saya menyetel cookie agar expire setelah 60 detik.
+?>
